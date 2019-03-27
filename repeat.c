70842f719413e2fa558d4502401957b95f6476cf
Unknown <luke@andajalu.com>
2019-03-27 12:50:40 -0400

    Fixed issue with repeat

--- repeat.c
+++ repeat.c
@@ -2,15 +2,27 @@
 //Lab 08
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "extlib.h"
 
-char *repeat(char *s, int n){
+char * repeat(char *s, int n){
 
-  for(int i=0; i<n; i++) //Loop n times and print string each time
-	  printf("%s", s);
-
-  return NULL;
+  int counter = 0;
+  char *holder = s;
+  char *buff = malloc(sizeof(char)*50);
+  for(int i=0; i<n; i++) //Loop n times
+  {
+	  //copy string to buffer each time
+	  while(*holder)
+	  {
+	    buff[counter++] = *holder;
+	    holder = holder + 1; 
+	  }  
+	holder = s;
+  }
+  buff[counter] = '\0';
+  return buff;
 }
 
 
@@ -18,13 +30,16 @@ char *repeat(char *s, int n){
 int main()
 {
   char b[] = {"Hello"};
+  char *a;
   int n;
   printf("ENTER A INTEGER VALUE: ");
   fflush(NULL);
   scanf("%d", &n); //Get n value
 
   printf("String is: %s\n", b);
-  repeat(b, n); //Call function
+  a = repeat(b, n); //Call function
+  printf("%s",a);
   fflush(NULL);
+
 }
 #endif 
